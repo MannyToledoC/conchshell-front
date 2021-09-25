8147aec6a5beccf4ec0031d88f9eef6bea25260f
Manny Toledo <mtole035@fiu.edu>
2021-09-25 14:05:25 -0400

    Lyrid changes

--- src/components/MessageBox/MessageBox.jsx
+++ src/components/MessageBox/MessageBox.jsx
@@ -1,25 +1,15 @@
+import { ListGroup } from "react-bootstrap";
 import Message from "./../Message";
 import MessageInput from "./../MessageInput";
 import "./MessageBox.scss";
-function MessageBox() {
+function MessageBox(props) {
+  let msg1 = { from: "from-username", text: "lorem ipsum..." };
+  let msgarray = [msg1, msg1, msg1, msg1];
   return (
     <div className="message-box">
-      <Message
-        from="client"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      />
-      <Message
-        from="bot"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      />
-      <Message
-        from="client"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      />
-      <Message
-        from="bot"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      />
+      {msgarray.map((msg) => (
+        <Message from={msg.from} text={msg.text} />
+      ))}
       <MessageInput />
     </div>
   );
